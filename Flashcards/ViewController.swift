27c9a5871f75be1eaf5166490b8d9bf2b843fe6b
Shagoon <twiggymarilyn00@gmail.com>
2020-02-21 00:44:54 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Shagoon on 2/21/20.
+//  Copyright © 2020 Shagoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
